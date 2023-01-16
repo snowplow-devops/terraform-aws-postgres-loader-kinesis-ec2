a01b2868aa4435880bcbe3db37b8194f58497728
jbeemster <joshua.a.beemster@gmail.com>
2023-01-16 16:38:19 +1100

    Increase memory allocation for application (closes #9)

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ data "aws_caller_identity" "current" {}
 
 module "telemetry" {
   source  = "snowplow-devops/telemetry/snowplow"
-  version = "0.2.0"
+  version = "0.3.0"
 
   count = var.telemetry_enabled ? 1 : 0
 
@@ -82,7 +82,7 @@ resource "aws_dynamodb_table" "kcl" {
 
 module "kcl_autoscaling" {
   source  = "snowplow-devops/dynamodb-autoscaling/aws"
-  version = "0.1.1"
+  version = "0.2.0"
 
   table_name = aws_dynamodb_table.kcl.id
 
@@ -255,6 +255,13 @@ resource "aws_security_group_rule" "rds_egress_tcp_webserver" {
 
 # --- EC2: Auto-scaling group & Launch Configurations
 
+module "instance_type_metrics" {
+  source  = "snowplow-devops/ec2-instance-type-metrics/aws"
+  version = "0.1.2"
+
+  instance_type = var.instance_type
+}
+
 locals {
   resolvers_raw = concat(var.default_iglu_resolvers, var.custom_iglu_resolvers)
 
@@ -332,6 +339,9 @@ locals {
 
     cloudwatch_logs_enabled   = var.cloudwatch_logs_enabled
     cloudwatch_log_group_name = local.cloudwatch_log_group_name
+
+    container_memory = "${module.instance_type_metrics.memory_application_mb}m"
+    java_opts        = var.java_opts
   })
 }
 
@@ -362,7 +372,7 @@ resource "aws_launch_configuration" "lc" {
 
 module "tags" {
   source  = "snowplow-devops/tags/aws"
-  version = "0.1.2"
+  version = "0.2.0"
 
   tags = local.tags
 }
